c65ec283859af120b9b28cf226b6c62dfa3ba829
Harshal Moon <2020bcs065S@sggs.ac.in>
2024-08-03 23:25:59 +0530

    Adding Components to Routes

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,15 @@
-import { Route, Routes, Router } from 'react-router-dom'
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Home from './pages/Home';
 import './stylesheet/home.css';
 import PageNotFound from './pages/PageNotFound';
+
 function App() {
   return (
     <div className="App">
       <Router>
         <Routes>
-          <Route path='/' exact component={Home} />
-          <Route path='/emptyPage' exact component={PageNotFound} />
+          <Route path="/" element={<Home />} />
+          <Route path="/emptyPage" element={<PageNotFound />} />
         </Routes>
       </Router>
     </div>
