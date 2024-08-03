aa3878d57885d144a868a36f8c769fd8b757b6ba
Harshal Moon <2020bcs065S@sggs.ac.in>
2024-08-03 23:12:15 +0530

    Adding Routes

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,16 @@
-import {Route,BrowserRouter} from 'react-router-dom'
+import { Route, Routes, Router } from 'react-router-dom'
 import Home from './pages/Home';
 import './stylesheet/home.css';
 import PageNotFound from './pages/PageNotFound';
 function App() {
   return (
     <div className="App">
-      <BrowserRouter>
-        <Route path='/' exact component={Home}/>
-        <Route path='/emptyPage' exact component={PageNotFound}/>
-      </BrowserRouter>
+      <Router>
+        <Routes>
+          <Route path='/' exact component={Home} />
+          <Route path='/emptyPage' exact component={PageNotFound} />
+        </Routes>
+      </Router>
     </div>
   );
 }
