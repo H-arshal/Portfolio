30e010bfb61651652b0a45f33c417f19257153c8
Harshal Moon <2020bcs065S@sggs.ac.in>
2025-05-01 15:04:47 +0530

    Updating resume and adding download button

--- client/src/components/MyPdfViewer.js
+++ client/src/components/MyPdfViewer.js
@@ -1,14 +1,50 @@
+// import React from 'react';
+// import { Worker, Viewer } from '@react-pdf-viewer/core';
+// import '@react-pdf-viewer/core/lib/styles/index.css';
+// import pdfUrl from '../resume/Harshal_Dev.pdf'; 
+
+// const PdfViewer = () => {
+//   return (
+//     <div style={{ height: '1110px' }}>
+//       <Worker workerUrl="https://cdnjs.cloudflare.com/ajax/libs/pdf.js/3.11.174/pdf.worker.min.js">
+//         <Viewer fileUrl={pdfUrl} />
+//       </Worker>
+//     </div>
+//   );
+// };
+
+// export default PdfViewer;
+
+
 import React from 'react';
 import { Worker, Viewer } from '@react-pdf-viewer/core';
 import '@react-pdf-viewer/core/lib/styles/index.css';
-import pdfUrl from '../resume/Resume_Updated.pdf'; 
+import pdfUrl from '../resume/Harshal_Dev.pdf';
 
 const PdfViewer = () => {
   return (
     <div style={{ height: '1110px' }}>
+
       <Worker workerUrl="https://cdnjs.cloudflare.com/ajax/libs/pdf.js/3.11.174/pdf.worker.min.js">
         <Viewer fileUrl={pdfUrl} />
       </Worker>
+
+      <div style={{ marginBottom: '10px', textAlign: 'right' }}>
+        <a
+          href={pdfUrl}
+          download="Harshal_Resume.pdf"
+          style={{
+            textDecoration: 'none',
+            backgroundColor: '#4CAF50',
+            color: 'white',
+            padding: '10px 20px',
+            borderRadius: '5px',
+            fontWeight: 'bold'
+          }}
+        >
+          Download Resume
+        </a>
+      </div>
     </div>
   );
 };
